5f3bb4e7af32ba3f4736fcfe8e534fa82cb30359
Robert Buchi <robert@planetweb.ro>
2025-02-11 22:00:24 +0200

    'v1.0.1'

--- vue/app/Support/Globals.js
+++
@@ -1,9 +0,0 @@
-export default async () => {
-    return {
-        Auth: Store.state.Auth,
-        FRONT_URL: import.meta.env.VITE_FRONT_URL,
-        BACK_URL: import.meta.env.VITE_BACK_URL,
-        UPLOADS_URL: import.meta.env.VITE_UPLOADS_URL,
-        IS_PRODUCTION: import.meta.env.VITE_ENV == 'production',
-    }
-}
