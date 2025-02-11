f74688c3644fc7a325f4756dc080a2b202567adf
Robert Buchi <robert@planetweb.ro>
2025-02-11 22:00:34 +0200

    'v1.0.1'

--- electron/after-pack.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs-extra';
-
-export default async () => {
-    await fs.unlink('dist/win-unpacked/resources/database/database.sqlite');
-}
--- laravel/routes/console.php
+++
@@ -1,5 +0,0 @@
-<?php
-use App\Models as Model;
-
-Artisan::command('test', function() {
-});
