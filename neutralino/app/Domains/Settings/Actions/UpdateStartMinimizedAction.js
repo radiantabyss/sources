920c9d02515bf4a317e744682ec755378537c724
Robert Buchi <robert@planetweb.ro>
2026-06-04 16:26:07 +0300

    'v1.0.1

--- neutralino/app/Domains/Settings/Actions/UpdateStartMinimizedAction.js
+++
@@ -1,8 +0,0 @@
-let self = {
-    async run() {
-        let data = Invoked.all();
-        await Storage.set('start_minimized', data.start_minimized);
-    },
-}
-
-export default self;
--- neutralino/app/Routes/routes.js
+++
@@ -1,9 +0,0 @@
-import Route from '@radiantabyss/neutralino/src/Routing/Route.js';
-
-Route.get('/dialog/select-path', 'Dialog/SelectPathAction');
-Route.post('/dialog/open-external-link', 'Dialog/OpenExternalLinkAction');
-
-Route.get('/settings/start-minimized', 'Settings/StartMinimizedAction');
-Route.post('/settings/update-start-minimized', 'Settings/UpdateStartMinimizedAction');
-Route.get('/settings/usage', 'Settings/UsageAction');
-Route.post('/settings/update-usage', 'Settings/UpdateUsageAction');
